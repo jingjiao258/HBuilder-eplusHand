7dd0e5b5b25fd7878b730204f153cdf52b80aa14
jingjiao258 <1035766456@qq.com>
2018-02-01 17:07:18 +0800

    Initial Commit

---
+++ HBuilder-Integrate/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  Pandora
+//
+//  Created by Mac Pro_C on 12-12-26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PDRCore.h"
+#import "PDRCoreAppWindow.h"
+
+@interface ViewController : UIViewController<PDRCoreDelegate,PDRCoreAppWindowDelegate>
+{
+  
+}
+
+
+@end
